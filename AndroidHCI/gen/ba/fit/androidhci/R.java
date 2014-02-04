fac0acfc58b4bd24da39bb3c986a9bc622f8545f
Nikola Kujaca <nikola.kujaca@gmail.com>
2014-02-04 11:20:20 +0100

    Rijesio search engine u php-u i dodalo login formu. Nije implementirana

--- AndroidHCI/gen/ba/fit/androidhci/R.java
+++ AndroidHCI/gen/ba/fit/androidhci/R.java
@@ -38,66 +38,89 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int profile=0x7f020004;
     }
     public static final class id {
-        public static final int buttonAllVehicles=0x7f090006;
-        public static final int buttonNewVehicle=0x7f090008;
-        public static final int buttonSearchVehicles=0x7f090007;
-        public static final int button_selectpic=0x7f09000a;
-        public static final int gallery=0x7f09001d;
-        public static final int image=0x7f090014;
-        public static final int imageView1=0x7f090017;
-        public static final int imageView_pic=0x7f090009;
-        public static final int imgLogo=0x7f090013;
+        public static final int action_forgot_password=0x7f090023;
+        public static final int buttonAllVehicles=0x7f09000d;
+        public static final int buttonNewVehicle=0x7f09000f;
+        public static final int buttonSearchVehicles=0x7f09000e;
+        public static final int button_selectpic=0x7f090011;
+        public static final int email=0x7f090008;
+        public static final int gallery=0x7f090025;
+        public static final int image=0x7f09001b;
+        public static final int imageView1=0x7f09001e;
+        public static final int imageView_pic=0x7f090010;
+        public static final int imgLogo=0x7f09001a;
         public static final int imgdesc=0x7f090002;
-        public static final int inputDesc=0x7f09000c;
-        public static final int inputName=0x7f09000b;
-        public static final int inputSearchTerm=0x7f090010;
-        public static final int listview=0x7f090005;
-        public static final int main=0x7f09001f;
-        public static final int messageText=0x7f09000e;
-        public static final int newvehicle=0x7f09001c;
-        public static final int pbar=0x7f09001b;
+        public static final int inputDesc=0x7f090013;
+        public static final int inputName=0x7f090012;
+        public static final int inputSearchTerm=0x7f090017;
+        public static final int listview=0x7f09000c;
+        public static final int login=0x7f09000a;
+        public static final int login_form=0x7f090007;
+        public static final int login_status=0x7f090005;
+        public static final int login_status_message=0x7f090006;
+        public static final int main=0x7f090027;
+        public static final int messageText=0x7f090015;
+        public static final int newvehicle=0x7f090024;
+        public static final int password=0x7f090009;
+        public static final int pbar=0x7f090022;
         public static final int pbardesc=0x7f090004;
-        public static final int search=0x7f09001e;
-        public static final int searchButton=0x7f090011;
-        public static final int tvMessage=0x7f090012;
-        public static final int tvProductName=0x7f09000f;
-        public static final int tvcreated=0x7f090016;
+        public static final int search=0x7f090026;
+        public static final int searchButton=0x7f090018;
+        public static final int sign_in_button=0x7f09000b;
+        public static final int tvMessage=0x7f090019;
+        public static final int tvProductName=0x7f090016;
+        public static final int tvcreated=0x7f09001d;
         public static final int tvdescription=0x7f090003;
         public static final int tvdescriptiontitle=0x7f090001;
-        public static final int tvname=0x7f090015;
+        public static final int tvname=0x7f09001c;
         public static final int tvregplate=0x7f090000;
-        public static final int tvtextc=0x7f090018;
-        public static final int tvtextu=0x7f090019;
-        public static final int tvupdated=0x7f09001a;
-        public static final int uploadButton=0x7f09000d;
+        public static final int tvtextc=0x7f09001f;
+        public static final int tvtextu=0x7f090020;
+        public static final int tvupdated=0x7f090021;
+        public static final int uploadButton=0x7f090014;
     }
     public static final class layout {
         public static final int activity_detail=0x7f030000;
-        public static final int activity_main_screen=0x7f030001;
-        public static final int activity_menu_screen=0x7f030002;
-        public static final int activity_new_vehicle=0x7f030003;
-        public static final int activity_search=0x7f030004;
-        public static final int activity_splashscreen=0x7f030005;
-        public static final int row=0x7f030006;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main_screen=0x7f030002;
+        public static final int activity_menu_screen=0x7f030003;
+        public static final int activity_new_vehicle=0x7f030004;
+        public static final int activity_search=0x7f030005;
+        public static final int activity_splashscreen=0x7f030006;
+        public static final int row=0x7f030007;
     }
     public static final class menu {
-        public static final int menu_list=0x7f080000;
+        public static final int login=0x7f080000;
+        public static final int menu_list=0x7f080001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f060018;
         public static final int action_settings=0x7f060002;
+        public static final int action_sign_in_register=0x7f060016;
+        public static final int action_sign_in_short=0x7f060017;
         public static final int app_name=0x7f060000;
         public static final int description=0x7f060004;
+        public static final int error_field_required=0x7f06001d;
+        public static final int error_incorrect_password=0x7f06001c;
+        public static final int error_invalid_email=0x7f06001a;
+        public static final int error_invalid_password=0x7f06001b;
         public static final int fitId=0x7f060007;
+        public static final int login_progress_signing_in=0x7f060019;
         public static final int main_screen=0x7f060012;
         public static final int novo_vozilo=0x7f060009;
         public static final int odaberi_sliku=0x7f060008;
         public static final int odabrana_slika=0x7f06000c;
         public static final int product_name=0x7f060003;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f060014;
+        public static final int prompt_password=0x7f060015;
         public static final int search_button_text=0x7f06000f;
         public static final int search_vehicles=0x7f060010;
         public static final int sva_vozila=0x7f06000a;
         public static final int title_activity_detail=0x7f06000d;
         public static final int title_activity_list_all=0x7f060005;
+        public static final int title_activity_login=0x7f060013;
         public static final int title_activity_menu_screen=0x7f060011;
         public static final int title_activity_new_vehicle=0x7f060001;
         public static final int title_activity_search=0x7f06000e;
@@ -109,11 +132,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
 
         
 
@@ -135,8 +160,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int AppTheme=0x7f070001;
         public static final int ButtonBar=0x7f070003;
         public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenActionBarStyle=0x7f070006;
         public static final int FullscreenTheme=0x7f070002;
+        public static final int LoginFormContainer=0x7f070005;
     }
     public static final class styleable {
         /** 
